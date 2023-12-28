a0c2c4f771a87f3f74e2711cc19befd2d9118c63
ravana09 <pradeep09sathish@gmail.com>
2023-12-28 13:59:57 +0530

    for loops task done

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+ let country = {
+    "india" :"ind",
+    "china": "chi",
+    "unites_state" : "usa",
+    "united_Kingdom ": "uk",
+    "antarticka" :"ak"
+  }
+
+  let countryKey =Object.keys(country);
+  let countryValues = Object.values(country)
+  
+  // for loop
+
+  for(let i=0;i<countryKey.length;i++){
+    console.log(` Name of the country : ${countryKey[i]},
+    Short simple of the country : ${countryValues[i]}
+    `)
+
+  }
+
+  // for in
+
+  for(key in country){
+    console.log(`
+    name opf the country :${key},
+    Short Form of country :${country[key]}
+    
+    `)
+  }
+
+  // for each
+  console.log("------------ for each_______________")
+
+  countryKey.forEach((values ) => {
+    console.log(` 
+    name opf the country :${values},
+    Short Form of country :${country[values]}
+    `)
+    
+  });
+
+  // for off
+
+  //let countryKey =Object.keys(country);
+
+  console.log("---------------for off----------------")
+  for(key of countryKey){
+    console.log(`
+    name opf the country :${key},
+    Short Form of country :${country[key]}
+    
+    `)
+  }
+
+ 
